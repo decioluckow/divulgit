e121da9a5fa8f23cd1ce092d2452c5b4189088ec
wesleyeduardodev <wesleyeduardo.dev@gmail.com>
2022-08-07 12:34:47 -0300

    iniciando ajuste de tratamento de erros

--- divulgit-remote-bitbucket/src/main/java/org/divulgit/bitbucket/error/BitBucketErrorResponseHandler.java
+++ divulgit-remote-bitbucket/src/main/java/org/divulgit/bitbucket/error/BitBucketErrorResponseHandler.java
@@ -15,20 +15,18 @@ import org.springframework.stereotype.Component;
 @ForRemote(RemoteType.BITBUCKET)
 public class BitBucketErrorResponseHandler implements ErrorResponseHandler {
 
-    //TODO avaliar esse método pois acho que não vai rolar pois o message está como subnível de error no json de erros
     @Override
     public boolean isErrorResponse(ResponseEntity<String> response) {
-        return ! response.getStatusCode().is2xxSuccessful();
+        return response.getStatusCode().is4xxClientError() || response.getStatusCode().is5xxServerError();
     }
 
     @Override
-    //TODO fazer tratasmento para casos onde nao retorna Json com detalhes do erro
     public void handleErrorResponse(ResponseEntity<String> response) throws RemoteException {
         try {
             final Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
             final ObjectMapper objectMapper = builder.build();
             ErrorMessage errorMessage =  objectMapper.readValue(response.getBody(), ErrorMessage.class);
-            //TODO pode ser que eu nao tenha a resposta do erro no Json, avaliar ver se tem ou nao, pode ysar o status code como auxilio
+            log.error(errorMessage.getError().getMessage().concat(" - ").concat(errorMessage.getError().getDetail()));
             throw new RemoteException(errorMessage.getError().getMessage());
         } catch (JsonProcessingException e) {
             String message = "Error on converting json to Object";
